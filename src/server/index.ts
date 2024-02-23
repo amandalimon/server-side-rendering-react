180bbd729f0e26ebf82cc7ecead4a4320d2e343b
Amanda Limón <amandalimon@outlook.com>
2024-02-22 19:23:43 -0600

    rendering react from the server side

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,11 @@
 import express, { Express, Request, Response } from 'express'
 import { config } from './config';
-import { template } from './render/template';
+import { render } from './render';
 
 const app: Express = express();
 
 app.get('*', (req: Request, res: Response) => {
-    res.send(template(`Hola desde la ruta: ${req.url}`))
+    res.send(render(req.url))
 });
 
 app.listen(config.PORT, () => {
